b777866a845598cddeff8a3a35c4bb4b84ec59b3
Joni Eskelinen <jonieske@gmail.com>
2016-05-03 07:54:35 +0300

    Set default timezone for tests

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,3 +2,7 @@
 
 define("TEST_ASSETS_DIR", __DIR__ . "/assets");
 require_once dirname(__DIR__) . "/vendor/autoload.php";
+
+if (empty(ini_get("date.timezone"))) {
+	ini_set("date.timezone", "UTC");
+}
