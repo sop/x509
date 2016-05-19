3a50deea5c8735d23e53551eabf04ca0ef9d8c7c
Joni Eskelinen <joni@mobie.fi>
2016-05-19 13:03:24 +0300

    Fix constructor method

--- lib/X509/CertificationPath/Policy/PolicyNode.php
+++ lib/X509/CertificationPath/Policy/PolicyNode.php
@@ -5,6 +5,12 @@ namespace X509\CertificationPath\Policy;
 use X509\Certificate\Extension\CertificatePolicy\PolicyInformation;
 
 
+/**
+ * Policy node class for certification path validation.
+ *
+ * @todo Implement
+ * @link https://tools.ietf.org/html/rfc5280#section-6.1.2
+ */
 class PolicyNode
 {
 	protected $_validPolicy;
@@ -13,7 +19,14 @@ class PolicyNode
 	
 	protected $_expectedPolicies;
 	
-	public function __constructor($valid_policy, array $qualifiers, 
+	/**
+	 * Constructor
+	 *
+	 * @param string $valid_policy Policy OID
+	 * @param array $qualifiers
+	 * @param array $expected_policies
+	 */
+	public function __construct($valid_policy, array $qualifiers, 
 			array $expected_policies) {
 		$this->_validPolicy = $valid_policy;
 		$this->_qualifiers = $qualifiers;
