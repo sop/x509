8c49675367bd35c7d6fd19d06c0a912224f7ac3e
Joni Eskelinen <joni@mobie.fi>
2016-07-06 14:09:37 +0300

    Add generic validation exception

--- lib/X509/CertificationPath/Exception/PathValidationException.php
+++ lib/X509/CertificationPath/Exception/PathValidationException.php
@@ -2,11 +2,13 @@
 
 namespace X509\CertificationPath\Exception;
 
+use X509\Exception\X509ValidationException;
+
 
 /**
  * Exception thrown on certification path validation errors.
  */
-class PathValidationException extends \RuntimeException
+class PathValidationException extends X509ValidationException
 {
 
 }
---
+++ lib/X509/Exception/X509ValidationException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace X509\Exception;
+
+
+class X509ValidationException extends \RuntimeException
+{
+
+}
